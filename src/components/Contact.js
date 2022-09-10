e78cc1be4e77e3d5c03b5794f9124b4d4ae09079
Hiresh <hbremanand4@gmail.com>
2022-09-10 18:16:47 +0100

    contact-form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,8 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import React from 'react';
-import sendMail from '../server'
 import { NavLink } from "react-router-dom";
-
+import {sendMail} from './helper/Mail';
 
 const Contact = () => {
   
@@ -20,8 +19,8 @@ const Contact = () => {
   }
 
   async function postContact(e) {
-    // e.preventDefault()
-
+    e.preventDefault()
+    // sendMail()
     // try{
     //   await axios.post("https://dead-red-swordfish-sock.cyclic.app/post_contact", {
     //     firstname,
@@ -32,6 +31,8 @@ const Contact = () => {
     // }catch(error){
     //   console.log(error)
     // }
+
+
   }
 
 
@@ -39,12 +40,11 @@ const Contact = () => {
     document.title = "Ansel Jobs | Contact Us";
   })
   
-  /*onSubmit={(e) => {postContact(e)}} */
   return (
     <>
       <section className='m-2'>
         <div className="bg-blue-800 w-[90%] m-auto flex flex-wrap flex-col font-poppins text-white shadow-2xl rounded mt-28">
-          {/* <form className='sm:max-w-xl max-w-xs m-auto text-[20px] mb-10'> 
+          {/* <form onSubmit={(e) => {postContact(e)}} className='sm:max-w-xl max-w-xs m-auto text-[20px] mb-10'> 
             <h1 className='ml-5 mt-8 text-[40px] mb-10'>Contact Form</h1>
             <input id="fName" value={firstname} required className="bg-transparent m-3 p-3 border-b-2 sm:w-[96%] w-[92%]" type="text" placeholder="Your First Name" onChange={(e)=> setFirstName(e.target.value)} onSubmit={(e) => e.target.value=""}/>
             <input id="lName" value={lastname} required className="bg-transparent m-3 p-3 border-b-2 sm:w-[96%] w-[92%]" type="text" placeholder="Your Last Name" onChange={(e)=> setLastName(e.target.value)} />
@@ -52,12 +52,21 @@ const Contact = () => {
             <textarea id="message" value={message} required className="bg-transparent border-2 rounded m-3 sm:w-[96%] w-[92%] h-40 p-3 mt-6" placeholder="Your Message" onChange={(e)=> setMessage(e.target.value)}></textarea>
             <button className="bg-white sm:w-[96%] w-[92%] m-3 rounded-md p-1 text-blue-800 font-poppins" type="submit">SUBMIT</button>
           </form> */}
-          <form name="contact" method="POST" netlify className='sm:max-w-xl max-w-xs m-auto text-[20px] mb-10'> 
+          <form name="contact v1" method="post" data-netlify="true" onSubmit="submit" className='sm:max-w-xl max-w-xs m-auto text-[20px] mb-10'> 
             <h1 className='ml-5 mt-8 text-[40px] mb-10'>Contact Form</h1>
-            <input type="hidden" name="form-firstname" value="contact" id="fName" required className="bg-transparent m-3 p-3 border-b-2 sm:w-[96%] w-[92%]" placeholder="Your First Name" />
-            <input type="hidden" name="form-lastname" value="contact" id="fName" required className="bg-transparent m-3 p-3 border-b-2 sm:w-[96%] w-[92%]" placeholder="Your Last Name" />
-            <input type="hidden" name="form-email" value="contact" id="email" required className="bg-transparent m-3 p-3 border-b-2 sm:w-[96%] w-[92%]" placeholder="Your Email" />
-            <textarea type="hidden" name="form-message" value="contact" id="message" required className="bg-transparent border-2 rounded m-3 sm:w-[96%] w-[92%] h-40 p-3 mt-6" placeholder="Your Message"></textarea>
+            <input type="hidden" name="form-name" value="contact v1" id="fName" required className="bg-transparent m-3 p-3 border-b-2 sm:w-[96%] w-[92%]" placeholder="Your First Name" />
+
+            <div>
+              <label>First name
+                <input type="text" id="first-name" className='text-black' />
+              </label>
+            </div>            
+
+            <div>
+              <label>Last name
+                <input type="text" id="last-name" className='text-black'/>
+              </label>
+            </div>            
             <button className="bg-white sm:w-[96%] w-[92%] m-3 rounded-md p-1 text-blue-800 font-poppins" type="submit">SUBMIT</button>
           </form>
         </div>
