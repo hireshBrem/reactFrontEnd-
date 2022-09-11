afef43d5523cec90e7ed158d471d590d74dacfc5
Hiresh <hbremanand4@gmail.com>
2022-09-11 13:38:17 +0100

    contact-form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -52,16 +52,16 @@ const Contact = () => {
             <textarea id="message" value={message} required className="bg-transparent border-2 rounded m-3 sm:w-[96%] w-[92%] h-40 p-3 mt-6" placeholder="Your Message" onChange={(e)=> setMessage(e.target.value)}></textarea>
             <button className="bg-white sm:w-[96%] w-[92%] m-3 rounded-md p-1 text-blue-800 font-poppins" type="submit">SUBMIT</button>
           </form> */}
-          <form name="contact" method="post">
+          <form name="contact" method="post" className='sm:max-w-xl max-w-xs m-auto text-[20px] mb-10'>
             <input type="hidden" name="form-name" value="contact" />
             <p>
-              <label>Your Name: <input type="text" name="name"/></label>
+              <label><input className="bg-transparent m-3 p-3 border-b-2 sm:w-[96%] w-[92%]" type="text" name="name" placeholder='Name'/></label>
             </p>
             <p>
-              <label>Your Email: <input type="email" name="email"/></label>
+              <label><input className="bg-transparent m-3 p-3 border-b-2 sm:w-[96%] w-[92%]" type="email" name="email" placeholder='Email'/></label>
             </p>
             <p>
-              <label>Message: <textarea name="message"></textarea></label>
+              <label><textarea className="bg-transparent border-2 rounded m-3 sm:w-[96%] w-[92%] h-40 p-3 mt-6" name="message" placeholder='Enter Your Comments'></textarea></label>
             </p>
             <p>
               <button type="submit">Send</button>
